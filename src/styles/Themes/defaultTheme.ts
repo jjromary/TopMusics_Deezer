358b803f2bc4c63abb03b19aa1bf3d342c1757b0
José Romary Brandão <joseromarybrandao@gmail.com>
2023-01-24 23:10:06 -0300

    define interface themes

--- src/styles/Themes/defaultTheme.ts
+++ src/styles/Themes/defaultTheme.ts
@@ -1,12 +1,16 @@
-export const defaultTheme = {
-  white: "#FFFFFF",
-  "blue900": "#041418",
-  "gray900": "#121214",
-  "gray800": "#202024",
-  "gray500": "#7C7C8A",
-  "gray300": "#c4c4cc",
-  "gray100": "#e1e1e6",
+interface IStringIndex {
+  [key: string]: any;
+}
 
-  "green500": "#00875f",
-  "green300": "#00b37e",
+export const defaultTheme: IStringIndex = {
+  white: "#FFFFFF",
+  blue900: "#041418",
+  gray900: "#121214",
+  gray800: "#202024",
+  gray500: "#7C7C8A",
+  gray300: "#c4c4cc",
+  gray100: "#e1e1e6",
+
+  green500: "#00875f",
+  green300: "#00b37e",
 } as const;
